44e858b74ef0579e1e8f7c9e3c79eba87fbbdf36
NorielRS <norielrs04@gmail.com>
2023-05-04 23:00:34 +0800

    add recursion example and edit previous project

--- youtube_&_coding_challenges/find_highest.py
+++ youtube_&_coding_challenges/find_highest.py
@@ -1,11 +1,20 @@
 def find_highest(given_list):
-    i = 1
-    highest = given_list[0]
-    if highest > given_list[i]:
-        return highest
-    else:
-        i+1
-        return find_highest(given_list)
+    print(len(given_list))
+
+
+
+    # i = 1
+    # highest = given_list[0]
+
+
+    # if len(given_list)>0:
+    #     return 
+
+    # if highest > given_list[i]:
+    #     print(highest)
+    # else:
+    #     i+1
+    #     return find_highest(given_list)
 
 
 
@@ -14,4 +23,40 @@ def find_highest(given_list):
 
 
 
-print(find_highest([0, 12, 4, 87]))
+find_highest([0, 12, 4, 87])
+
+
+
+# # Recursion example
+# def tri_recursion(k):
+#   if(k>0):
+#     result = k+tri_recursion(k-1)
+#     print(result)
+#   else:
+#     result = 0
+#   return result
+
+# print("\n\nRecursion Example Results")
+# tri_recursion(5)
+
+
+# # Program to print factorial of a number
+# # recursively.
+
+# # Recursive function
+# def recursive_factorial(n):
+#     if n == 1:
+#         return n
+#     else:
+#         return n * recursive_factorial(n-1)
+
+# # user input
+# num = 6
+
+# # check if the input is valid or not
+# if num < 0:
+#     print("Invalid input ! Please enter a positive number.")
+# elif num == 0:
+#     print("Factorial of number 0 is 1")
+# else:
+#     print("Factorial of number", num, "=", recursive_factorial(num))
