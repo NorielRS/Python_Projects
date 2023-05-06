652f85ea4f6fb27fb16990d02511bd79e958ab60
NorielRS <norielrs04@gmail.com>
2023-05-06 13:32:33 +0800

    optimized code for same_parity.py challlenge

--- youtube_&_coding_challenges/same_paraty.py
+++ youtube_&_coding_challenges/same_paraty.py
@@ -1,9 +1,16 @@
 def paraty_analysis(number):
 
-    toSeperate = str(number)
+    # toSeperate = str(number)
     totalFinal = 0
-    for i in range(len(toSeperate)):
-        totalFinal += int(toSeperate[i])
+    # for i in range(len(toSeperate)):
+    #     totalFinal += int(toSeperate[i])
+
+    for i in range(len(str(number))):
+        totalFinal += int(str(number)[i])
+
+
+
+
 
     if number % 2 == 0 and totalFinal % 2 == 0 or number % 2 == 1 and totalFinal % 2 == 1:
         return 'True'  
